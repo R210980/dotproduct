646e3b524e04351b638070a66398e683571aed32
R210980 <159228280+R210980@users.noreply.github.com>
2024-05-01 15:01:40 +0530

    Add files via upload

---
+++ dot_product.m
@@ -0,0 +1,4 @@
+v1 = [1,2,3];
+v2 = [4,5,6];
+dot_product = sum(v1 .* v2);
+disp(dot_product);
---
+++ dot_product.py
@@ -0,0 +1,6 @@
+#importing numpy module
+import numpy as np
+#taking two 2D arrays
+x=[[2,1],[0,3]]
+y=[[1,1],[3,2]]
+print(np.dot(x,y))
